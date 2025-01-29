d413210a14d3f0c7f6d89175ec055adf3b416dde
Riiel.edu <riven18@farmingdale.edu>
2025-01-29 10:13:29 -0500

    created classes

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,33 @@
-public class Car {
+public class Car extends Vehicle {
+    //Initializing new variable
+    private String brand;
 
+    //New constructors
+    public Car() { }
+
+    public Car(int inNum, String inCol, float inEn, String inFuel, String inBrand) {
+        this.numberOfWheels = inNum;
+        this.color = inCol;
+        this.engineSize = inEn;
+        this.fuelType = inFuel;
+        this.brand = inBrand;
+    }
+
+    //New setter and getter
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String inBrand) {
+        this.brand = inBrand;
+    }
+
+    //new methods
+    public void honk() {
+        System.out.println("Honk, honk!");
+    }
+
+    public void displayInfo() {
+        System.out.println("Number of Wheels: " + this.numberOfWheels + "/nColor: " + this.color + "/nEngine Size: " + this.engineSize + "/nFuelType: " + "/nBrand: " + this.brand);
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,5 +1,52 @@
 public class Vehicle {
+    //creating the variables
+    protected int numberOfWheels;
+    protected String color;
+    protected float engineSize;
+    protected String fuelType;
 
     //Initializing the empty class constructor
     public Vehicle() { }
+
+    //Class constructor with all attributes
+    public Vehicle(int inNum, String inCol, float inEn, String inFuel) {
+        this.numberOfWheels = inNum;
+        this.color = inCol;
+        this.engineSize = inEn;
+        this.fuelType = inFuel;
+    }
+
+    //Getter methods
+    public int getNumberOfWheels() {
+        return this.numberOfWheels;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public float getEngineSize() {
+        return this.engineSize;
+    }
+
+    public String getFuelType() {
+        return this.fuelType;
+    }
+
+    //Setter methods
+    public void setNumberOfWheels(int inNum) {
+        this.numberOfWheels = inNum;
+    }
+
+    public void setColor(String inCol) {
+        this.color = inCol;
+    }
+
+    public void setEngineSize(float inEn) {
+        this.engineSize = inEn;
+    }
+
+    public void setFuelType(String inFuel) {
+        this.fuelType = inFuel;
+    }
 }
